17c913f4330de1cff1602ffcb6392df887458ad2
fabioser-ai <fabioser@gmail.com>
2026-04-16 15:45:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown(
     """
     <style>
-    .stApp {
+    [data-testid="stAppViewContainer"] {
         background-image: url("https://images.unsplash.com/photo-1581091215367-59ab6b4c3c69");
         background-size: cover;
         background-position: center;
