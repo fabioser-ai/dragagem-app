54882c91d2510becbcb2728bf0a6835e8389e4f1
fabioser-ai <fabioser@gmail.com>
2026-04-16 18:16:18 -0300

    Update codigos.py

--- services/codigos.py
+++ services/codigos.py
@@ -1,1 +1,20 @@
+from datetime import datetime
+import re
 
+def gerar_codigo_obra(df):
+
+    ano = datetime.now().year
+
+    if df.empty:
+        return f"D_001_{ano}"
+
+    numeros = []
+
+    for val in df["Codigo"]:
+        match = re.search(r"D_(\d+)_", str(val))
+        if match:
+            numeros.append(int(match.group(1)))
+
+    proximo = max(numeros) + 1 if numeros else 1
+
+    return f"D_{str(proximo).zfill(3)}_{ano}"
