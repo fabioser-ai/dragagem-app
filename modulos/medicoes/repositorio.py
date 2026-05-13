68d1df23c76f35702a50bff0cef02129dd8463f2
fabioser-ai <fabioser@gmail.com>
2026-05-13 19:00:30 -0300

    Update repositorio.py

--- modulos/medicoes/repositorio.py
+++ modulos/medicoes/repositorio.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import os
 
 from services.github import carregar_github, salvar_github
 
@@ -18,6 +19,10 @@ from modulos.medicoes.config import (
     COL_SERVICOS,
 )
 
+from modulos.medicoes.config import (
+    ARQ_TABELAS_SERVICOS_DIR,
+    COL_TABELA_SERVICOS_CONTRATO,
+)
 
 # ============================================================
 # CSV
@@ -104,3 +109,41 @@ def carregar_bases():
         itens,
         servicos,
     )
+
+# ============================================================
+# Carregar tabela contrato
+# ============================================================
+
+
+def carregar_tabela_contrato(nome_arquivo):
+    if not nome_arquivo:
+        return pd.DataFrame(
+            columns=COL_TABELA_SERVICOS_CONTRATO
+        )
+
+    caminho = (
+        f"{ARQ_TABELAS_SERVICOS_DIR}/{nome_arquivo}"
+    )
+
+    if not os.path.exists(caminho):
+        return pd.DataFrame(
+            columns=COL_TABELA_SERVICOS_CONTRATO
+        )
+
+    try:
+        df = pd.read_csv(caminho)
+
+        for c in COL_TABELA_SERVICOS_CONTRATO:
+            if c not in df.columns:
+                df[c] = None
+
+        return df[COL_TABELA_SERVICOS_CONTRATO]
+
+    except Exception as e:
+        st.error(
+            f"Erro ao carregar tabela contratual: {e}"
+        )
+
+        return pd.DataFrame(
+            columns=COL_TABELA_SERVICOS_CONTRATO
+        )
